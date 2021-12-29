23b1d0203e01a5cabf3a5f49ca903b9548cc8851
jianfeng <jianfeng@sap.com>
2021-12-29 10:56:36 +0800

    help

--- jianfeng/examples/MyCoolDrawer.java
+++ jianfeng/examples/MyCoolDrawer.java
@@ -43,4 +43,44 @@ public class MyCoolDrawer {
         sc.close();
     }
 
+    public void PrintCircle() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Radius : ");
+        int r = sc.nextInt();
+        System.out.print("Enter Symbol : ");
+        char c = sc.next().charAt(0);
+        sc.close();
+
+        int offset = 2;
+
+        int x0 = r + offset;
+        int y0 = r + offset;
+
+        for (int y = 1; y <= r * 2 + offset; y++) {
+            for (int x = 1; x <= r * 2 + offset; x++) {
+                boolean isPrintedChar = false;
+                if (x >= offset && x <= 2 * r + offset && y >= offset && y <= r * 2 + offset) {
+                    if (r * (r - 1) <= (x - x0) * (x - x0) + (y - y0) * (y - y0)
+                            && (x - x0) * (x - x0) + (y - y0) * (y - y0) <= r * (r + 1)) {
+                                isPrintedChar = true;
+                    }
+                }
+
+                if (isPrintedChar) {
+                    PrintChar(c);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void PrintRectangle(){
+        
+    }
+    private boolean PrintChar(char c) {
+        System.out.print(c);
+        return true;
+    }
 }
--- jianfeng/examples/MyCoolJava.java
+++ jianfeng/examples/MyCoolJava.java
@@ -3,6 +3,8 @@ package jianfeng.examples;
 public class MyCoolJava {
     public static void main(String[] args) {
         MyCoolDrawer drawer = new MyCoolDrawer();
-        drawer.PrintDiamond();
+        //drawer.PrintDiamond();
+        drawer.PrintCircle();
+
     }
 }
